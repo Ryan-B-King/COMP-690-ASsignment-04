34c6588aa19c11dedd338c26b23a2df40a3f782e
Ryan King <33941531+Ryan-B-King@users.noreply.github.com>
2023-03-07 09:50:00 -0800

    Starting files

---
+++ js/01-functions-practice.js
@@ -0,0 +1,33 @@
+//STEP 1
+
+let testNumber = Int(prompt('Enter a number:'));
+
+
+function halfNumber(arg){
+    arg = arg / 2;
+    return arg;
+}
+
+console.log ('Half of ' + testNumber + ' is ' + arg + '.')
+
+
+
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+
+
+
+
+
+
+
+// Practice with Functions (5 points)
+
+// Write a function called halfNumber() that accepts one argument (a number), divide it by 2, and return the result. It should log a string like "Half of 5 is 2.5.".
+// Write a function called squareNumber() that accepts one argument (a number), square that number, and return the result. It should also log a string like "The result of squaring the number 3 is 9."
+// Write a function called percentOf() that accepts two numbers, figure out what percent the first number represents of the second number, and return the result. It should log a string like "2 is 50% of 4."
+// Write a function called findModulus() that accepts two numbers. Within the function write a statement that returns the modulus of the first and second parameters. It should log a string like "2 is the modulus of 4 and 10."
