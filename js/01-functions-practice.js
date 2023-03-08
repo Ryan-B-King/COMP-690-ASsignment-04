4cd09b76bbd44c63b18a2bed13839d866a38e549
Ryan King <33941531+Ryan-B-King@users.noreply.github.com>
2023-03-07 17:55:50 -0800

    updated assignment

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,21 +1,62 @@
 //STEP 1
 
-// let testNumber = Int(prompt('Enter a number:'));
+// let startNumber = parseInt(prompt('Enter a number:'));
 
+// halfNumber(startNumber);
 
-// function halfNumber(arg){
-//     arg = arg / 2;
-//     return arg;
-// }
+// function halfNumber(){
 
-// console.log ('Half of ' + testNumber + ' is ' + arg + '.')
+//     let half = startNumber / 2;
+//     return console.log ('Half of ' + startNumber + ' is ' + half + '.');
+
+// };
 
 
 //STEP 2
 
+// let startNumber = parseInt(prompt('Enter a number:'));
+
+// squaredNumber(startNumber);
+
+// function squaredNumber(){
+
+//      let squared = startNumber*startNumber;
+//      return console.log ('The result of squaring the number ' + startNumber + ' is ' + squared + '.');
+
+// };
+
+
 //STEP 3
 
+// let startNumber01 = parseInt(prompt('Enter your first number:'));
+// let startNumber02 = parseInt(prompt('Enter your second number:'));
+
+// percentNumber(startNumber01, startNumber02);
+
+// function percentNumber(one, two){
+
+//      let percent = (one/two)*100;
+//      return console.log (startNumber01 + ' is ' + percent + '% of ' + startNumber02 + '.');
+
+// };
+
+
 //STEP 4
+
+let startNumber01 = parseInt(prompt('Enter your first number:'));
+let startNumber02 = parseInt(prompt('Enter your second number:'));
+
+modulusNumber(startNumber01, startNumber02);
+
+function modulusNumber(one, two){
+
+     let modulus = (one % two);
+     return console.log (modulus + ' is the modulus of ' + startNumber01 + ' and ' + startNumber02 + '.');
+
+};
+
+
+
 
 
 // Practice with Functions (5 points)
